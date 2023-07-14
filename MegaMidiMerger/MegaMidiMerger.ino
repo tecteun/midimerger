6f0aae11d28c60af6e43a0e572184250b23ee97a
tecteun <tecteun@hotmail.com>
2023-07-14 23:04:20 +0200

    update

--- MegaMidiMerger/MegaMidiMerger.ino
+++ MegaMidiMerger/MegaMidiMerger.ino
@@ -41,7 +41,7 @@ midi::MidiInterface<uhs2Midi::uhs2MidiTransport>* list_devices_uhs2[MIDI_UHS2_DE
 
 bool toggle = false;
 
-void send_uhs( midi::MidiType t, midi::DataByte d1, midi::DataByte d2, midi::Channel ch, int exclude = -1) {
+void send_uhs(midi::MidiType t, midi::DataByte d1, midi::DataByte d2, midi::Channel ch, int exclude = -1) {
   for (int i = 0; i < MIDI_UHS2_DEVICE_COUNT; i++) {
     if (exclude != i) {  // do not send to self, no passthrough
       list_devices_uhs2[i]->send(t, d1, d2, ch);
@@ -58,14 +58,20 @@ void send_serial(midi::MidiType t, midi::DataByte d1, midi::DataByte d2, midi::C
 }
 
 void eurorack_trigger() {
-  send_serial(midi::NoteOn, 42, 127, 1);    // Send a Note (pitch 42, velo 127 on channel 1)
-  send_uhs(midi::NoteOn, 42, 127, 1);    // Send a Note (pitch 42, velo 127 on channel 1)
-  delay(1000);		            // Wait for a second
-  send_serial(midi::NoteOff, 42, 0, 1);    // Send a NoteOff (pitch 42, velo 0 on channel 1)
-  send_uhs(midi::NoteOff, 42, 0, 1);    // Send a NoteOff (pitch 42, velo 0 on channel 1) 
+  send_serial(midi::NoteOn, 42, 127, 1);  // Send a Note (pitch 42, velo 127 on channel 1)
+  send_uhs(midi::NoteOn, 42, 127, 1);     // Send a Note (pitch 42, velo 127 on channel 1)
+  delay(1000);                            // Wait for a second
+  send_serial(midi::NoteOff, 42, 0, 1);   // Send a NoteOff (pitch 42, velo 0 on channel 1)
+  send_uhs(midi::NoteOff, 42, 0, 1);      // Send a NoteOff (pitch 42, velo 0 on channel 1)
 }
 
 void setup() {
+  pinMode(5, OUTPUT);  // Blue Pin
+  pinMode(6, OUTPUT);  // Green Pin
+  pinMode(7, OUTPUT);  // Red Pin
+  analogWrite(5, 2);
+  analogWrite(6, 2);
+  analogWrite(7, 2);
   // interrupts
   pinMode(EURORACK_TRIGGER_INTERRUPT_PIN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(EURORACK_TRIGGER_INTERRUPT_PIN), eurorack_trigger, CHANGE);  //LOW RISING FALLING
@@ -94,28 +100,49 @@ void setup() {
   delay(200);
 }
 
+unsigned long previousMillis = 0;  // will store last time LED was updated
+
+int pwm = -1;
+int pwmValue = 10;
+int ledbrightness = 5;
 void loop() {
   Usb.Task();
+  
+  unsigned long currentMillis = millis();
+  
+  if (currentMillis - previousMillis >= 100) {
+    previousMillis = currentMillis;
+    analogWrite(7, pwmValue = pwmValue + pwm);
+    if (pwmValue <= 0 || pwmValue > 10) {
+      pwm *= -1;
+    }
+    analogWrite(6, 0);
+    analogWrite(5, 0);
+    ledbrightness = 5;
+  }
+
   for (int c = 0; c < MIDI_SERIAL_DEVICE_COUNT; c++) {
     if (list_devices_serial[c]->read()) {
+      analogWrite(5, ledbrightness += ledbrightness);
       digitalWrite(LED_BUILTIN, toggle = !toggle ? LOW : HIGH);
       midi::MidiType t = list_devices_serial[c]->getType();
       midi::DataByte d1 = list_devices_serial[c]->getData1();
       midi::DataByte d2 = list_devices_serial[c]->getData2();
       midi::Channel ch = list_devices_serial[c]->getChannel();
-      send_serial(t, d1, d2, ch, c); // do not send to self, no passthrough
+      send_serial(t, d1, d2, ch, c);  // do not send to self, no passthrough
       send_uhs(t, d1, d2, ch);
     }
   }
   for (int c = 0; c < MIDI_UHS2_DEVICE_COUNT; c++) {
     if (list_devices_uhs2[c]->read()) {
+      analogWrite(6, ledbrightness += ledbrightness);
       digitalWrite(LED_BUILTIN, toggle = !toggle ? LOW : HIGH);
       midi::MidiType t = list_devices_uhs2[c]->getType();
       midi::DataByte d1 = list_devices_uhs2[c]->getData1();
       midi::DataByte d2 = list_devices_uhs2[c]->getData2();
       midi::Channel ch = list_devices_uhs2[c]->getChannel();
-      send_serial(t, d1, d2, ch); 
-      send_uhs(t, d1, d2, ch, c); // do not send to self, no passthrough
+      send_serial(t, d1, d2, ch);
+      send_uhs(t, d1, d2, ch, c);  // do not send to self, no passthrough
     }
   }
 }
