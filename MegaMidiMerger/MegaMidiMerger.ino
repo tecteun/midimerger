aa57f0291d9a40897ae347cc7452a22e653bf1ca
tecteun <tecteun@hotmail.com>
2023-07-14 20:34:48 +0200

    add devices, use for;;

--- MegaMidiMerger/MegaMidiMerger.ino
+++ MegaMidiMerger/MegaMidiMerger.ino
@@ -4,6 +4,9 @@
 //https://github.com/TheKikGen/USBMidiKliK
 //send sysex F0 77 77 77 09 F7 to reset interface to serial mode, to flash
 
+
+
+
 USB Usb;
 
 // support one hub, four midi devices
@@ -12,151 +15,87 @@ UHS2MIDI_CREATE_INSTANCE(&Usb, 0, midiUsb);
 UHS2MIDI_CREATE_INSTANCE(&Usb, 1, midiUsb1);
 UHS2MIDI_CREATE_INSTANCE(&Usb, 2, midiUsb2);
 UHS2MIDI_CREATE_INSTANCE(&Usb, 3, midiUsb3);
+UHS2MIDI_CREATE_INSTANCE(&Usb, 4, midiUsb4);
+UHS2MIDI_CREATE_INSTANCE(&Usb, 5, midiUsb5);
+UHS2MIDI_CREATE_INSTANCE(&Usb, 6, midiUsb6);
+UHS2MIDI_CREATE_INSTANCE(&Usb, 7, midiUsb7);
+UHS2MIDI_CREATE_INSTANCE(&Usb, 8, midiUsb8);
+UHS2MIDI_CREATE_INSTANCE(&Usb, 9, midiUsb9);
+#define MIDI_UHS2_DEVICE_COUNT 10
 
-MIDI_CREATE_INSTANCE(HardwareSerial, Serial,     midiUsbMidiKlik);
-MIDI_CREATE_INSTANCE(HardwareSerial, Serial1,    midiA);
-MIDI_CREATE_INSTANCE(HardwareSerial, Serial2,    midiB);
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial, midiUsbMidiKlik);
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, midiA);
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial2, midiB);
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial3, midiC);
+#define MIDI_SERIAL_DEVICE_COUNT 4
+
+midi::MidiInterface<midi::SerialMIDI<HardwareSerial>>* list_devices_serial[MIDI_SERIAL_DEVICE_COUNT] = {
+  &midiUsbMidiKlik, &midiA, &midiB, &midiC
+};
+
+midi::MidiInterface<uhs2Midi::uhs2MidiTransport>* list_devices_uhs2[MIDI_UHS2_DEVICE_COUNT] = {
+  &midiUsb, &midiUsb1, &midiUsb2, &midiUsb3, &midiUsb4, &midiUsb5, &midiUsb6, &midiUsb7, &midiUsb8, &midiUsb9
+};
+
 bool toggle = false;
-void setup()
-{
+void setup() {
   // Make Arduino transparent for serial communications from and to USB(client-hid)
-  pinMode(0,INPUT); // Arduino RX - ATMEGA8U2 TX
-  pinMode(1,INPUT); // Arduino TX - ATMEGA8U2 RX
+  pinMode(0, INPUT);  // Arduino RX - ATMEGA8U2 TX
+  pinMode(1, INPUT);  // Arduino TX - ATMEGA8U2 RX
   Serial.begin(31250);
-  
+
   pinMode(LED_BUILTIN, OUTPUT);
   // Initiate MIDI communications, listen to all channels
 
-  midiUsbMidiKlik.begin(MIDI_CHANNEL_OMNI);
-  midiA.begin(MIDI_CHANNEL_OMNI);
-  midiB.begin(MIDI_CHANNEL_OMNI);  
-  midiUsb.begin(MIDI_CHANNEL_OMNI);
-  midiUsb1.begin(MIDI_CHANNEL_OMNI);
-  midiUsb2.begin(MIDI_CHANNEL_OMNI);
-  midiUsb3.begin(MIDI_CHANNEL_OMNI);
-  
-  midiUsbMidiKlik.turnThruOff();
-  midiA.turnThruOff();
-  midiB.turnThruOff();
-  midiUsb.turnThruOff();
-  midiUsb1.turnThruOff();
-  midiUsb2.turnThruOff();
-  midiUsb3.turnThruOff();
+  for (int i = 0; i < MIDI_SERIAL_DEVICE_COUNT; i++) {
+    list_devices_serial[i]->begin(MIDI_CHANNEL_OMNI);
+    list_devices_serial[i]->turnThruOff();
+  }
+  for (int i = 0; i < MIDI_UHS2_DEVICE_COUNT; i++) {
+    list_devices_uhs2[i]->begin(MIDI_CHANNEL_OMNI);
+    list_devices_uhs2[i]->turnThruOff();
+  }
 
   if (Usb.Init() == -1) {
-    while (1); //halt
-  }//if (Usb.Init() == -1...
-  delay( 200 );
+    while (1);  //halt
+  }      //if (Usb.Init() == -1...
+  delay(200);
 }
 
-void loop()
-{
+void loop() {
   Usb.Task();
-  
-  if (midiUsbMidiKlik.read())
-  {
-    digitalWrite(LED_BUILTIN, toggle = !toggle ? LOW : HIGH);
-    midi::MidiType t = midiUsbMidiKlik.getType();
-    midi::DataByte d1 = midiUsbMidiKlik.getData1();
-    midi::DataByte d2 = midiUsbMidiKlik.getData2();
-    midi::Channel  c = midiUsbMidiKlik.getChannel();
-
-    midiA.send(t, d1, d2, c);
-    midiB.send(t, d1, d2, c);
-    midiUsb.send(t, d1, d2, c);
-    midiUsb1.send(t, d1, d2, c);
-    midiUsb2.send(t, d1, d2, c);
-    midiUsb3.send(t, d1, d2, c);
+  for (int c = 0; c < MIDI_SERIAL_DEVICE_COUNT; c++) {
+    if (list_devices_serial[c]->read()) {
+      digitalWrite(LED_BUILTIN, toggle = !toggle ? LOW : HIGH);
+      midi::MidiType t = list_devices_serial[c]->getType();
+      midi::DataByte d1 = list_devices_serial[c]->getData1();
+      midi::DataByte d2 = list_devices_serial[c]->getData2();
+      midi::Channel ch = list_devices_serial[c]->getChannel();
+      for (int i = 0; i < MIDI_SERIAL_DEVICE_COUNT; i++) {
+        if (c == i) {  // do not send to self, no passthrough
+          list_devices_serial[i]->send(t, d1, d2, ch);
+        }
+      }
+      for (int i = 0; i < MIDI_UHS2_DEVICE_COUNT; i++) {
+        list_devices_uhs2[i]->send(t, d1, d2, ch);
+      }
+    }
   }
-  
-  if (midiUsb.read())
-  {
-    digitalWrite(LED_BUILTIN, toggle = !toggle ? LOW : HIGH);
-    midi::MidiType t = midiUsb.getType();
-    midi::DataByte d1 = midiUsb.getData1();
-    midi::DataByte d2 = midiUsb.getData2();
-    midi::Channel  c = midiUsb.getChannel();
-
-    midiUsbMidiKlik.send(t, d1, d2, c);
-    midiA.send(t, d1, d2, c);
-    midiB.send(t, d1, d2, c);
-    midiUsb1.send(t, d1, d2, c);
-    midiUsb2.send(t, d1, d2, c);
-    midiUsb3.send(t, d1, d2, c);
+  for (int c = 0; c < MIDI_UHS2_DEVICE_COUNT; c++) {
+    if (list_devices_uhs2[c]->read()) {
+      digitalWrite(LED_BUILTIN, toggle = !toggle ? LOW : HIGH);
+      midi::MidiType t = list_devices_uhs2[c]->getType();
+      midi::DataByte d1 = list_devices_uhs2[c]->getData1();
+      midi::DataByte d2 = list_devices_uhs2[c]->getData2();
+      midi::Channel ch = list_devices_uhs2[c]->getChannel();
+      for (int i = 0; i < MIDI_SERIAL_DEVICE_COUNT; i++) {
+        list_devices_serial[i]->send(t, d1, d2, ch);
+      }
+      for (int i = 0; i < MIDI_UHS2_DEVICE_COUNT; i++) {
+        if (c == i) {  // do not send to self, no passthrough
+          list_devices_uhs2[i]->send(t, d1, d2, ch);
+        }
+      }
+    }
   }
-
-  if (midiUsb1.read())
-  {
-    digitalWrite(LED_BUILTIN, toggle = !toggle ? LOW : HIGH);
-    midi::MidiType t = midiUsb1.getType();
-    midi::DataByte d1 = midiUsb1.getData1();
-    midi::DataByte d2 = midiUsb1.getData2();
-    midi::Channel  c = midiUsb1.getChannel();
-
-    midiUsbMidiKlik.send(t, d1, d2, c);
-    midiA.send(t, d1, d2, c);
-    midiB.send(t, d1, d2, c);
-    midiUsb.send(t, d1, d2, c);
-    midiUsb2.send(t, d1, d2, c);
-    midiUsb3.send(t, d1, d2, c);
-  }
-
-  if (midiUsb2.read())
-  {
-    digitalWrite(LED_BUILTIN, toggle = !toggle ? LOW : HIGH);
-    midi::MidiType t = midiUsb2.getType();
-    midi::DataByte d1 = midiUsb2.getData1();
-    midi::DataByte d2 = midiUsb2.getData2();
-    midi::Channel  c = midiUsb2.getChannel();
-
-    midiUsbMidiKlik.send(t, d1, d2, c);
-    midiA.send(t, d1, d2, c);
-    midiB.send(t, d1, d2, c);
-    midiUsb.send(t, d1, d2, c);
-    midiUsb1.send(t, d1, d2, c);
-    midiUsb3.send(t, d1, d2, c);
-  }
-
-  if (midiUsb3.read())
-  {
-    digitalWrite(LED_BUILTIN, toggle = !toggle ? LOW : HIGH);
-    midi::MidiType t = midiUsb3.getType();
-    midi::DataByte d1 = midiUsb3.getData1();
-    midi::DataByte d2 = midiUsb3.getData2();
-    midi::Channel  c = midiUsb3.getChannel();
-
-    midiUsbMidiKlik.send(t, d1, d2, c);
-    midiA.send(t, d1, d2, c);
-    midiB.send(t, d1, d2, c);
-    midiUsb.send(t, d1, d2, c);
-    midiUsb1.send(t, d1, d2, c);
-    midiUsb2.send(t, d1, d2, c);
-  }
-  
-  if (midiA.read())
-  {
-    digitalWrite(LED_BUILTIN, toggle = !toggle ? LOW : HIGH);
-    midi::MidiType t = midiA.getType();
-    midi::DataByte d1 = midiA.getData1();
-    midi::DataByte d2 = midiA.getData2();
-    midi::Channel  c = midiA.getChannel();
-
-    midiUsbMidiKlik.send(t, d1, d2, c);
-    midiB.send(t, d1, d2, c);
-    midiUsb.send(t, d1, d2, c);
-  }
-  
-  if (midiB.read())
-  {
-    digitalWrite(LED_BUILTIN, toggle = !toggle ? LOW : HIGH);
-    midi::MidiType t = midiB.getType();
-    midi::DataByte d1 = midiB.getData1();
-    midi::DataByte d2 = midiB.getData2();
-    midi::Channel  c = midiB.getChannel();
-
-    midiUsbMidiKlik.send(t, d1, d2, c);
-    midiA.send(t, d1, d2, c);
-    midiUsb.send(t, d1, d2, c);
-  }
-  
 }
