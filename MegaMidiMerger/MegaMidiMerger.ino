c9018d5f9e428a0900f5302e2f1bc778da21ae57
tecteun <tecteun@hotmail.com>
2023-07-17 12:50:12 +0200

    support sysex

--- MegaMidiMerger/MegaMidiMerger.ino
+++ MegaMidiMerger/MegaMidiMerger.ino
@@ -4,13 +4,13 @@
 //https://github.com/TheKikGen/USBMidiKliK
 //send sysex F0 77 77 77 09 F7 to reset interface to serial mode, to flash
 
-
-
-
+// @see https://github.com/diyelectromusic/sdemp/blob/HEAD/src/SDEMP/ArduinoMultiMIDIMerge2/ArduinoMultiMIDIMerge2.ino
 USB Usb;
-
 // support one hub, four midi devices
 USBHub Hub(&Usb);
+USBHub Hub1(&Usb);
+USBHub Hub2(&Usb);
+USBHub Hub3(&Usb);
 UHS2MIDI_CREATE_INSTANCE(&Usb, 0, midiUsb);
 UHS2MIDI_CREATE_INSTANCE(&Usb, 0, midiUsb1);
 UHS2MIDI_CREATE_INSTANCE(&Usb, 0, midiUsb2);
@@ -51,11 +51,29 @@ void send_uhs(midi::MidiType t, midi::DataByte d1, midi::DataByte d2, midi::Chan
     }
   }
 }
+void send_uhs_sysex(midi::DataByte d1, midi::DataByte d2, const byte* sysexArray, int exclude = -1) {
+  for (int i = 0; i < MIDI_UHS2_DEVICE_COUNT; i++) {
+    if (exclude != i) {  // do not send to self, no passthrough
+      int mSxLen = d1 + 256 * d2;
+      list_devices_uhs2[i]->sendSysEx(mSxLen, sysexArray, true);
+    }
+  }
+}
+
 
 void send_serial(midi::MidiType t, midi::DataByte d1, midi::DataByte d2, midi::Channel ch, int exclude = -1) {
   for (int i = 0; i < MIDI_SERIAL_DEVICE_COUNT; i++) {
     if (exclude != i) {  // do not send to self, no passthrough
       list_devices_serial[i]->send(t, d1, d2, ch);
+    }
+  }
+}
+
+void send_serial_sysex(midi::DataByte d1, midi::DataByte d2, const byte* sysexArray, int exclude = -1) {
+  for (int i = 0; i < MIDI_SERIAL_DEVICE_COUNT; i++) {
+    if (exclude != i) {  // do not send to self, no passthrough
+      int mSxLen = d1 + 256 * d2;
+      list_devices_serial[i]->sendSysEx(mSxLen, sysexArray, true);
     }
   }
 }
@@ -77,7 +95,6 @@ void setup() {
   pinMode(0, INPUT);  // Arduino RX - ATMEGA8U2 TX
   pinMode(1, INPUT);  // Arduino TX - ATMEGA8U2 RX
   Serial.begin(31250);
-
   pinMode(LED_BUILTIN, OUTPUT);
   // Initiate MIDI communications, listen to all channels
 
@@ -95,19 +112,18 @@ void setup() {
       ;  //halt
   }      //if (Usb.Init() == -1...
   delay(200);
-
 }
-void flashLed(){
+void flashLed() {
   toggle = true;
 }
 void loop() {
   Usb.Task();
-  
+
   unsigned long currentMillis = millis();
-  if(currentMillis - previousMillis >= 10){
+  if (currentMillis - previousMillis >= 10) {
     previousMillis = currentMillis;
     digitalWrite(LED_BUILTIN, toggle ? HIGH : LOW);
-    if(toggle){
+    if (toggle) {
       toggle = false;
     }
   }
@@ -115,23 +131,40 @@ void loop() {
   for (int c = 0; c < MIDI_SERIAL_DEVICE_COUNT; c++) {
     if (list_devices_serial[c]->read()) {
       flashLed();
+
       midi::MidiType t = list_devices_serial[c]->getType();
-      midi::DataByte d1 = list_devices_serial[c]->getData1();
-      midi::DataByte d2 = list_devices_serial[c]->getData2();
-      midi::Channel ch = list_devices_serial[c]->getChannel();
-      send_serial(t, d1, d2, ch, c);  // do not send to self, no passthrough
-      send_uhs(t, d1, d2, ch);
+      if (t != midi::SystemExclusive) {
+        midi::DataByte d1 = list_devices_serial[c]->getData1();
+        midi::DataByte d2 = list_devices_serial[c]->getData2();
+        midi::Channel ch = list_devices_serial[c]->getChannel();
+        send_serial(t, d1, d2, ch, c);  // do not send to self, no passthrough
+        send_uhs(t, d1, d2, ch);
+      } else {
+        byte* sysexArray = list_devices_serial[c]->getSysExArray();
+        midi::DataByte d1 = list_devices_serial[c]->getData1();
+        midi::DataByte d2 = list_devices_serial[c]->getData2();
+        send_serial_sysex(d1, d2, sysexArray, c);  // do not send to self, no passthrough
+        send_uhs_sysex(d1, d2, sysexArray);
+      }
     }
   }
   for (int c = 0; c < MIDI_UHS2_DEVICE_COUNT; c++) {
     if (list_devices_uhs2[c]->read()) {
       flashLed();
       midi::MidiType t = list_devices_uhs2[c]->getType();
-      midi::DataByte d1 = list_devices_uhs2[c]->getData1();
-      midi::DataByte d2 = list_devices_uhs2[c]->getData2();
-      midi::Channel ch = list_devices_uhs2[c]->getChannel();
-      send_serial(t, d1, d2, ch);
-      send_uhs(t, d1, d2, ch, c);  // do not send to self, no passthrough
+      if (t != midi::SystemExclusive) {
+        midi::DataByte d1 = list_devices_uhs2[c]->getData1();
+        midi::DataByte d2 = list_devices_uhs2[c]->getData2();
+        midi::Channel ch = list_devices_uhs2[c]->getChannel();
+        send_serial(t, d1, d2, ch);
+        send_uhs(t, d1, d2, ch, c);  // do not send to self, no passthrough
+      } else {
+        byte* sysexArray = list_devices_uhs2[c]->getSysExArray();
+        midi::DataByte d1 = list_devices_uhs2[c]->getData1();
+        midi::DataByte d2 = list_devices_uhs2[c]->getData2();
+        send_serial_sysex(d1, d2, sysexArray);
+        send_uhs_sysex(d1, d2, sysexArray, c);  // do not send to self, no passthrough
+      }
     }
   }
 }
